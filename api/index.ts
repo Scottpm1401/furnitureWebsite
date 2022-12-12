58f7b430a5991a4fc1a362f6796aba3d699c7851
NhatPhan <nhat.phan@executionlab.asia>
2022-12-12 15:56:45 +0700

    add interceptor

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+export const API = {
+  USER: {
+    LOGIN: '/user/login',
+    SIGNUP: '/user/signup',
+    LOGOUT: '/user/logout',
+    GETUSER: (id: string) => `/user/get/${id}`,
+    GETSELF: '/user/getself',
+    GETALL: '/user/all',
+    UPDATESELF: '/user/update_self',
+    DELETEUSER: (id: string) => `/user/delete/${id}`,
+    UPDATEUSER: (id: string) => `/user/update/${id}`,
+  },
+  PRODUCT: {
+    GETALL: '/product/all',
+    GETPRODUCT: (id: string) => `/product/get/${id}`,
+    CREATEPRODUCT: '/product/create',
+    UPDATEPRODUCT: (id: string) => `/product/update/${id}`,
+    DELETEPRODUCT: (id: string) => `/product/delete/${id}`,
+    RATINGPRODUCT: (id: string) => `/product/rating/${id}`,
+  },
+  PAYMENT: {
+    CHECKOUT: '/payment/checkout',
+    CONFIRM: '/payment/confirm',
+  },
+  REFRESHTOKEN: '/user/refreshToken',
+};
