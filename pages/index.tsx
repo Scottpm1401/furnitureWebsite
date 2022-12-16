ab4f5a5f10ad95d9243ab32de2c1bbae818dfb0a
NhatPhan <nhat.phan@executionlab.asia>
2022-12-16 14:15:52 +0700

    fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,21 @@ import { InferGetStaticPropsType } from 'next';
 import useTranslation from 'next-translate/useTranslation';
 import React from 'react';
 
+import { API } from '../api';
 import Newsletter from '../components/Home/Newsletter';
 import Section1 from '../components/Home/Section1';
 import Section2 from '../components/Home/Section2';
 import Section3 from '../components/Home/Section3';
+import axiosClient from '../interceptor';
 import { getFeaturedProduct } from '../services/product';
 
 type Props = {};
 
 export const getStaticProps = async () => {
-  const featuredProducts = await getFeaturedProduct();
+  const { data } = await axiosClient.get(API.PRODUCT.GETFEATURED);
   return {
     props: {
-      featuredProducts,
+      featuredProducts: data,
     },
   };
 };
--- services/product.ts
+++ services/product.ts
@@ -4,6 +4,5 @@ import { ProductType } from '../models/product';
 
 export const getFeaturedProduct = async () => {
   const res = await axiosClient.get(API.PRODUCT.GETFEATURED);
-  return [];
-  // return res.data as ProductType[];
+  return res.data as ProductType[];
 };
