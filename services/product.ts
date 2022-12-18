ef2d519c6c6c99266353af395e426781cf6d0b74
NhatPhan <nhat.phan@executionlab.asia>
2022-12-18 15:58:19 +0700

    fix build

--- services/product.ts
+++ services/product.ts
@@ -5,9 +5,9 @@ import axiosClient from '../interceptor';
 import { ProductType } from '../models/product';
 
 export const getFeaturedProduct = async () => {
-  // const res = await axiosClient.get(API.PRODUCT.GETFEATURED);
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_BE_URL}${API.PRODUCT.GETFEATURED}`
-  );
+  const res = await axiosClient.get(API.PRODUCT.GETFEATURED);
+  // const res = await axios.get(
+  //   `${process.env.NEXT_PUBLIC_BE_URL}${API.PRODUCT.GETFEATURED}`
+  // );
   return res.data as ProductType[];
 };
