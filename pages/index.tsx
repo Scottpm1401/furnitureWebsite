ee1e97f586dee7885e5fdeb5062f127e62ef0cac
NhatPhan <nhat.phan@executionlab.asia>
2023-01-01 02:36:31 +0700

    add auth to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
-import { Flex } from '@chakra-ui/react';
 import { InferGetStaticPropsType } from 'next';
-import useTranslation from 'next-translate/useTranslation';
 import React from 'react';
 
 import Newsletter from '../components/Home/Newsletter';
 import Section1 from '../components/Home/Section1';
 import Section2 from '../components/Home/Section2';
 import Section3 from '../components/Home/Section3';
+import NotAuthProvider from '../layout/NotAuthProvider';
 import Page from '../layout/Page';
 import { getFeaturedProduct } from '../services/product';
-
-type Props = {};
 
 export const getStaticProps = async () => {
   const featuredProducts = await getFeaturedProduct();
@@ -24,15 +21,15 @@ export const getStaticProps = async () => {
 const Home = ({
   featuredProducts,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { t } = useTranslation();
-
   return (
-    <Page direction='column'>
-      <Section1 />
-      <Section2 products={featuredProducts} />
-      <Section3 />
-      <Newsletter />
-    </Page>
+    <NotAuthProvider>
+      <Page direction='column'>
+        <Section1 />
+        <Section2 products={featuredProducts} />
+        <Section3 />
+        <Newsletter />
+      </Page>
+    </NotAuthProvider>
   );
 };
 
